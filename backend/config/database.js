b9586bb5becc90ddaa0e0b35294d8093c7c9db56
Joyce Kimojino-Weiss <jkimojinoweiss@gmail.com>
2025-02-26 13:04:07 -0600

    changing how database is being accessed

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,8 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variables: 'DATABASE_URL',
+    // use_env_variables: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
